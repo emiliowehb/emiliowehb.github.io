4b5065f920d9d39d2f9cf5067493f27554f4a347
Emilio Wehbe <wehbemile@hotmail.com>
2022-06-21 03:01:19 +0300

    verification for submit button fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const thankState = document.querySelector('#thankState');
 const rateState = document.querySelector('#rateState');
 const submitBtn = document.querySelector('#submit');
+const feedbackP = document.querySelector('.feedbackP');
 const thankSpan = document.querySelector('.thankSpan');
 let ratingSelected;
 
@@ -10,9 +11,16 @@ const ratingBtn = document.querySelectorAll('.ratingBtn');
 for(let button of ratingBtn){
     button.addEventListener('click', () => {
         ratingSelected = button.innerText;
+        submitBtn.addEventListener('click', ()=>{
+            document.body.replaceChild(thankState, rateState);
+            thankSpan.innerText = `You selected ${ratingSelected} out of 5`
+        })
     })
 }
+
 submitBtn.addEventListener('click', ()=>{
-    document.body.replaceChild(thankState, rateState);
-    thankSpan.innerText = `You selected ${ratingSelected} out of 5`
-})
+    const newAlert = document.createElement('span');
+    newAlert.innerHTML = '<b>Please select a rating level to submit!</b>';
+    newAlert.classList.add('warning');
+    feedbackP.append(newAlert);
+}, {once: true});
